4c81fa5bf8e2c6f375b7b3f0280229de317662b2
Markiian Sanotskyi <marksanockiy@gmail.com>
2022-05-17 19:04:46 -0400

    Pages built

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import {FaHome} from 'react-icons/fa'
+import {Link} from 'react-router-dom'
 
 function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className='hero'>
+        <div className="text-center hero-content">
+            <div className="max-w-lg">
+                <h1 className="text-8xl font-bold mb-8">
+                    Oops!
+                </h1>
+                <p className="text-5xl mb-8">
+                    404 - page not found!
+                </p>
+                <Link className=' btn btn-primary btn-lg' to='/'>
+                    <FaHome class='mr-2' />
+                    Back To Home
+                     </Link>
+            </div>
+        </div>
+    </div>
   )
 }
 
