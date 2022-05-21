a4c1f7cf832822819b781136bf750a79e887f97a
Markiian Sanotskyi <marksanockiy@gmail.com>
2022-05-21 15:56:13 -0400

    Implementing optimization

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -16,28 +16,6 @@ export const GithubProvider = ({ children }) => {
 
    const [state, dispatch] = useReducer(githubReducer, initialState)
 
-// GET SEARCH RESULTS
-
-    const searchUsers = async(text) => {
-        setLoading()
-
-        const params = new URLSearchParams({
-            q: text
-        })
-
-        const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-            headers: {
-                Authorization: `token ${GITHUB_TOKEN}`
-            }
-        })
-
-        const {items} = await response.json()
-
-       dispatch({
-           type: 'GET_USERS',
-            payload: items,
-       })
-    }
 
 
     // GET  Single user
@@ -105,11 +83,8 @@ export const GithubProvider = ({ children }) => {
     }
 
     return <GithubContext.Provider value={{
-        users: state.users,
-        loading: state.loading,
-        user: state.user,
-        repos: state.repos,
-        searchUsers,
+        ...state,
+        dispatch,
         clearUsers,
         getUser,
         getUserRepos,
